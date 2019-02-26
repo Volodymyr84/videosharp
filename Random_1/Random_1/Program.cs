6e578a4dc1263f68b0ae134c0f5a2822443a3d48
Natali <Natali@DIVCHUSKO>
2019-02-26 23:08:38 +0200

    Find expression value: 1 - 2 + 3 - 4 + 5-6 + 7 - 8 + 9 - 10 ...

---
+++ Random_1/Random_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// Найти значение выражения: 1 - 2 + 3 - 4 + 5-6 + 7 - 8 + 9 - 10 ... 
+/// Если в нём N слагаемых.
+/// Число N от 1 до 123456789.
+/// Начальные данные: первая строка ввода содержит число N.
+/// Вывод результата: вывод должен содержать одно число – ответ на задачу.
+/// </summary>
+namespace Random_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.Write("Enter number N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int rez = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                    rez -= i;
+                else
+                    rez += i;
+
+            }
+            Console.WriteLine(rez);
+            Console.ReadKey();
+        }
+    }
+}
